36ee849444b0e9a3d96c7b4e986cf6e317dcd37e
Ruslan Akbashev <ruslashev@gmail.com>
2019-12-08 22:40:29 +0300

    bf: get result

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn check_bf_syntax(src: &String) -> bool {
     for instr in src.chars() {
         balance +=
             match instr {
-                '[' => -1,
-                ']' => 1,
+                '[' => 1,
+                ']' => -1,
                 _ => 0
             }
     }
@@ -20,10 +20,36 @@ fn check_bf_syntax(src: &String) -> bool {
     balance == 0
 }
 
-fn find_next_closing_bracket(instr_ptr: usize, src: &String) -> usize {
+fn find_matching_closing_bracket(instr_ptr: usize, src: &String) -> usize {
+    let mut balance = 0;
+
     for (idx, instr) in src[instr_ptr..].chars().enumerate() {
         if instr == ']' {
-            return idx
+            if balance == 0 {
+                return idx
+            } else {
+                balance -= 1
+            }
+        } else if instr == '[' {
+            balance += 1
+        }
+    }
+
+    panic!("found no matching bracket but should");
+}
+
+fn find_matching_opening_bracket(instr_ptr: usize, src: &String) -> usize {
+    let mut balance = 0;
+
+    for (idx, instr) in src[..instr_ptr].chars().rev().enumerate() {
+        if instr == '[' {
+            if balance == 0 {
+                return idx
+            } else {
+                balance += 1
+            }
+        } else if instr == ']' {
+            balance -= 1
         }
     }
 
@@ -35,6 +61,7 @@ fn interpret_brainfuck(src: &String, max_intructions: u64) -> Result<String, BfE
     let mut tape_ptr: usize = 0;
     let mut tape: [u8; 1000] = [0; 1000];
     let mut num_instructions = 0;
+    let mut output = String::from("");
 
     if check_bf_syntax(src) == false {
         return Err(BfErr::SyntaxError)
@@ -61,22 +88,32 @@ fn interpret_brainfuck(src: &String, max_intructions: u64) -> Result<String, BfE
             '<' => { tape_ptr -= 1;       instr_ptr + 1 },
             '[' =>
                 if tape[tape_ptr] == 0 {
-                    find_next_closing_bracket(instr_ptr, src)
+                    find_matching_closing_bracket(instr_ptr, src)
                 } else {
                     instr_ptr + 1
                 },
+            ']' =>
+                if tape[tape_ptr] != 0 {
+                    find_matching_opening_bracket(instr_ptr, src)
+                } else {
+                    instr_ptr + 1
+                },
+            '.' => { output.push(tape[tape_ptr] as char); instr_ptr + 1 },
             _   => return Err(BfErr::SyntaxError)
         }
     }
 
-    Err(BfErr::SyntaxError)
+    Ok(output)
 }
 
 fn main() {
     let example_src = String::from("+++++++++++++++++++++++++++++++++.");
 
-    interpret_brainfuck(&example_src, INSTR_LIMIT);
-
-    println!("Hello, world!");
+    let res = interpret_brainfuck(&example_src, INSTR_LIMIT);
+    match res {
+        Ok(output)                     => println!("{}", output),
+        Err(BfErr::SyntaxError)        => println!("syntax error"),
+        Err(BfErr::InstrLimitExceeded) => println!("instr limit exceeded"),
+    }
 }
 
