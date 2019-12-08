f0150b6c923b74b0073c37c93ef3917701748cc5
Ruslan Akbashev <ruslashev@gmail.com>
2019-12-09 02:02:04 +0300

    add rand crate and adding simple genetic algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+use rand::Rng;
 mod bf;
 
-const INSTR_LIMIT: u64 = 10_000;
+const target: &str = "yummy cummy in my tummy";
+const initial_population_size : u64 = 100;
+const mutation_prob: f64 = 0.05;
+const elitism_ratio : f64 = 1. / 10.;
+const can_breed_ratio : f64 = 1. / 2.;
+
+type Gene = char;
+
+type Chromosome = Vec<Gene>;
+
+struct Individual {
+    chromosome: Chromosome,
+    fitness: u64
+}
+
+type Population = Vec<Individual>;
+
+macro_rules! rand_in_range {
+    ($min:expr, $max:expr) => {
+        rand::thread_rng().gen_range($min, $max)
+    }
+}
+
+fn random_gene() -> Gene {
+    let valid_genes: String = String::from("abcdefghijklmnopqrstuvwxyz\
+    ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}");
+
+    return valid_genes.as_bytes()[rand_in_range!(0, valid_genes.len() - 1)] as char;
+}
 
 fn main() {
-    let example_src = String::from("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
 
-    let res = bf::interpret_brainfuck(&example_src, INSTR_LIMIT);
-    match res {
-        Ok(output)                     => println!("{}", output),
-        Err(bf::BfErr::SyntaxError)        => println!("syntax error"),
-        Err(bf::BfErr::InstrLimitExceeded) => println!("instr limit exceeded"),
-    }
 }
 
