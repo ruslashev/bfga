bd972e9e79db1e098d7a276f330feab5e0bc1c4a
Ruslan Akbashev <ruslashev@gmail.com>
2019-12-08 22:29:45 +0300

    bf: add opening bracket

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,66 @@ enum BfErr {
     InstrLimitExceeded,
 }
 
-fn interpret_brainfuck(src: &String, max_intructions: u64) -> Result<String, BfErr> {
-    let mut num_instructions = 0;
-    let mut tape_ptr: usize = 0;
-    let mut tape: [u8; 1000] = [0; 1000];
+fn check_bf_syntax(src: &String) -> bool {
+    let mut balance = 0;
 
-    fn check_syntax(src: &String) -> bool {
-        let mut balance = 0;
-
-        for instr in src.chars() {
-            balance +=
-                match instr {
-                    '[' => -1,
-                    ']' => 1,
-                    _ => 0
-                }
-        }
-
-        balance == 0
+    for instr in src.chars() {
+        balance +=
+            match instr {
+                '[' => -1,
+                ']' => 1,
+                _ => 0
+            }
     }
 
-    if check_syntax(src) == false {
+    balance == 0
+}
+
+fn find_next_closing_bracket(instr_ptr: usize, src: &String) -> usize {
+    for (idx, instr) in src[instr_ptr..].chars().enumerate() {
+        if instr == ']' {
+            return idx
+        }
+    }
+
+    panic!("found no matching bracket but should");
+}
+
+fn interpret_brainfuck(src: &String, max_intructions: u64) -> Result<String, BfErr> {
+    let mut instr_ptr: usize = 0;
+    let mut tape_ptr: usize = 0;
+    let mut tape: [u8; 1000] = [0; 1000];
+    let mut num_instructions = 0;
+
+    if check_bf_syntax(src) == false {
         return Err(BfErr::SyntaxError)
     }
 
-    for instr in src.chars() {
+    loop {
+        if instr_ptr >= src.len() {
+            break
+        }
+
         num_instructions += 1;
         if num_instructions > max_intructions {
             return Err(BfErr::InstrLimitExceeded)
         }
 
-        println!("token: {}", instr);
+        let instr: char = src.as_bytes()[instr_ptr] as char;
 
-        match instr {
-            '+' => tape[tape_ptr] += 1,
-            '-' => tape[tape_ptr] -= 1,
-            '>' => tape_ptr += 1,
-            '<' => tape_ptr -= 1,
+        println!("{}: {}", instr_ptr, instr);
+
+        instr_ptr = match instr {
+            '+' => { tape[tape_ptr] += 1; instr_ptr + 1 },
+            '-' => { tape[tape_ptr] -= 1; instr_ptr + 1 },
+            '>' => { tape_ptr += 1;       instr_ptr + 1 },
+            '<' => { tape_ptr -= 1;       instr_ptr + 1 },
+            '[' =>
+                if tape[tape_ptr] == 0 {
+                    find_next_closing_bracket(instr_ptr, src)
+                } else {
+                    instr_ptr + 1
+                },
             _   => return Err(BfErr::SyntaxError)
         }
     }
