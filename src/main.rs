6a495100241f3cf9eea74b267d4a3f13341ed58f
Ruslan Akbashev <ruslashev@gmail.com>
2019-12-08 21:25:15 +0300

    beginning of brainfuck interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+const INSTR_LIMIT: u64 = 100_000;
+
+enum BfErr {
+    SyntaxError,
+    InstrLimitExceeded,
+}
+
+fn interpret_brainfuck(src: &String, max_intructions: u64) -> Result<String, BfErr> {
+    let mut num_instructions = 0;
+    let tape: [u8; 1000];
+
+    for instr in src.chars() {
+        num_instructions += 1;
+        println!("token: {}", instr);
+    }
+
+    Err(BfErr::SyntaxError)
+}
+
 fn main() {
+    let example_src = String::from("+++++++++++++++++++++++++++++++++.");
+
+    interpret_brainfuck(&example_src, INSTR_LIMIT);
+
     println!("Hello, world!");
 }
 
